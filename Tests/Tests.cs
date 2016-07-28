aa1b8c765bc4094adae9258f442c9165cbe73ae9
Vladimir Yangurskiy <software@ttrider.com>
2016-07-27 18:45:17 -0700

    moving to CoreCLR

---
+++ Tests/Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+using Xunit;
+using TTRider.Test;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestSuccess()
+        {
+            using (var context = TestDatabaseContext.Create("test", cnn =>
+            {
+                var command = new SqlCommand("SELECT * FROM sys.objects", cnn);
+                command.ExecuteNonQuery();
+            }))
+            {
+                context.Initialize(cnn =>
+                {
+                    var command = new SqlCommand("SELECT * FROM sys.tables", cnn);
+                    command.ExecuteNonQuery();
+
+                });
+
+
+            }
+
+
+
+        }
+    }
+}
